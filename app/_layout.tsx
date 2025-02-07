6a58b0265ff83ff9db0e7dacc373b9f04a8844db
Prince Eze <pchidireeze@gmail.com>
2025-02-07 19:05:25 +0100

    feat: update user interface

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,16 @@
 import { Stack } from "expo-router";
+import * as NavigationBar from "expo-navigation-bar";
+import { useEffect } from "react";
 
 export default function RootLayout() {
+  useEffect(() => {
+    const setBar = async () => {
+      await NavigationBar.setBackgroundColorAsync("#55555500");
+      await NavigationBar.setPositionAsync("absolute");
+    };
+    setBar();
+  }, []);
+
   return (
     <Stack>
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
